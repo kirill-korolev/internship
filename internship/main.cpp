06c08b7916b9988c5299cde63e61dbc73e61a269
kirill-korolev <kafafyf@gmail.com>
2018-02-09 12:49:12 +0300

    Initial Commit

---
+++ internship/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  internship
+//
+//  Created by Kirill Korolev on 09/02/2018.
+//  Copyright © 2018 Kirill Korolev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
